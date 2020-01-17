b8f4d3d41df8d83262b5cfc4ae331642574752c2
IStoleThePants <59883304+IStoleThePants@users.noreply.github.com>
2020-01-17 14:27:00 -0700

    Add files via upload

---
+++ Converter.java
@@ -0,0 +1,23 @@
+
+public class Converter {
+    //Your names go here:
+    /*
+     * @Author: Nurullo Boboev
+     * Bhavdeep Purba
+     * Trevor Brown
+     *
+     */
+    private double celsiusToFahrenheit(double C){
+// TODO: The third student will implement this method
+        return 0;
+    }
+    private double fahrenheitToCelsius(double F){
+        // TODO: The second student will implement this method
+        return 0;
+    }
+    public static void main(String[] args) {
+//TODO: The first student will implement this method.
+// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+    }
+}
