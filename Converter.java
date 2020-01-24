936010679149a61c247b15796a933c7c48b1856d
bhavdeep30 <bhavdeeppurba@gmail.com>
2020-01-23 17:42:36 -0700

    Add files via upload

--- Converter.java
+++ Converter.java
@@ -18,6 +18,10 @@ public class Converter {
 		double C = (F − 32) × 5/9;
         return C;
     }
+	private double kilometersToMiles(double k){
+		double m = k * (5/8);
+		return m;
+	}
     public static void main(String[] args) {
 //TODO: The first student will implement this method.
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
